4c8037bdb8a4a13b2d02503d9b185b5087604698
Vicky <vickygupta9987@gmail.com>
2023-04-30 22:47:24 +0530

    working on reference

--- index.js
+++ index.js
@@ -1,12 +1,56 @@
-import * as readline from "node:readline";
-import { stdin as input, stdout as output } from "node:process";
+import readline from "readline";
+import { spawn } from "child_process";
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
+const outputData = (output) => {
+  process.stdout.write(
+    `${output} \n\x1b[30m\x1b[36m${process.cwd()} \x1b[32mTerminal> \x1b[0m`
+  );
+};
+
+const parseInput = (inputs) => {
+  const args = inputs.split(" ");
+  const cmd = args[0];
+
+  switch (cmd) {
+    case "exit":
+      commands.exit();
+      break;
+    case "cd":
+      commands.cd(args.slice(1));
+    default:
+      break;
+  }
+};
+
+const commands = {
+  exit: () => {
+    process.exit(1);
+  },
+  cd: (path) => {
+    try {
+      process.chdir(path[0]);
+      outputData("Directory changed");
+    } catch (err) {
+      outputData(err.msg);
+    }
+  },
+};
+
+process.stdout.write(
+  `\x1b[30m\x1b[36m${process.cwd()} \x1b[32mTerminal> \x1b[0m`
+);
+
+process.stdin.on("data", (args) => {
+  args = args.toString().trim();
+  parseInput(args);
 });
 
-rl.question("What is your name?", (answer) => {
-  console.log(`Hello, ${answer}!`);
-  rl.close();
+process.on("SIGINT", function () {
+  console.log("\nKilling child process");
+  outputData("");
+});
+
+process.on("SIGTSTP", function () {
+  console.log("\nPutting child process to background");
+  outputData("");
 });
